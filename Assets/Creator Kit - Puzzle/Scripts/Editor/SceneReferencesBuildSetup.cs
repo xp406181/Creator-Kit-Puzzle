bb5b35f6b85f6e9e3c6ebabbed6ca42847388bb7
RunningSnail <xp406181@163.com>
2019-08-29 21:02:24 +0800

    初始化仓库

---
+++ Assets/Creator Kit - Puzzle/Scripts/Editor/SceneReferencesBuildSetup.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class SceneReferencesBuildSetup
+{
+    static SceneReferencesBuildSetup ()
+    {
+        EditorApplication.playModeStateChanged += PlayModeStateChanged;
+    }
+    
+    static void PlayModeStateChanged (PlayModeStateChange playModeStateChange)
+    {
+        if (playModeStateChange == PlayModeStateChange.ExitingEditMode)
+        {
+            List<string> uniqueScenePaths = GetScenePathsFromSceneReferences (out SceneReference[] sceneReferences);
+            
+            EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[uniqueScenePaths.Count];
+
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                scenes[i] = new EditorBuildSettingsScene(uniqueScenePaths[i], true);
+            }
+
+            EditorBuildSettings.scenes = scenes;
+
+            for (int i = 0; i < sceneReferences.Length; i++)
+            {
+                CacheInfo (sceneReferences[i]);
+            }
+        }
+    }
+
+    static List<string> GetScenePathsFromSceneReferences (out SceneReference[] sceneReferences)
+    {
+        List<string> uniqueScenePaths = new List<string> ();
+
+        sceneReferences = GetAllSceneReferences ();
+        
+        IgnoreNullScenes (sceneReferences);
+
+        for (int i = 0; i < sceneReferences.Length; i++)
+        {
+            SceneReference sceneReference = sceneReferences[i];
+            
+            if(sceneReference.ignore)
+                continue;
+            
+            string path = AssetDatabase.GetAssetOrScenePath (sceneReference.menuScene);
+            if(!uniqueScenePaths.Contains (path))
+                uniqueScenePaths.Add (path);
+            path = AssetDatabase.GetAssetOrScenePath (sceneReference.levelScene);
+            if(!uniqueScenePaths.Contains (path))
+                uniqueScenePaths.Add (path);
+        }
+
+        return uniqueScenePaths;
+    }
+
+    public static SceneReference[] GetAllSceneReferences ()
+    {
+        string[] guids = AssetDatabase.FindAssets ("t:SceneReference");
+        SceneReference[] sceneReferences = new SceneReference[guids.Length];
+
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath (guids[i]);
+            sceneReferences[i] = AssetDatabase.LoadAssetAtPath<SceneReference> (path);
+        }
+
+        return sceneReferences;
+    }
+
+    static void IgnoreNullScenes (SceneReference[] sceneReferences)
+    {
+        for (int i = 0; i < sceneReferences.Length; i++)
+        {
+            if (sceneReferences[i].menuScene == null || sceneReferences[i].levelScene == null)
+                sceneReferences[i].ignore = true;
+        }
+    }
+    
+    static void CacheInfo (SceneReference sceneReference)
+    {
+        if(sceneReference.ignore)
+            return;
+        
+        string levelPath = AssetDatabase.GetAssetOrScenePath (sceneReference.levelScene);
+            
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            if (EditorBuildSettings.scenes[i].path == levelPath)
+                sceneReference.levelBuildIndex = i;
+        }
+
+        string menuPath = AssetDatabase.GetAssetOrScenePath (sceneReference.menuScene);
+        
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            if (EditorBuildSettings.scenes[i].path == menuPath)
+                sceneReference.menuBuildIndex = i;
+        }
+    }
+}
---
+++ Assets/Creator Kit - Puzzle/Scripts/SceneSystems/SceneCompletion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneCompletion : MonoBehaviour
+{
+    public ScreenFader screenFader;
+    public GameObject panel;
+    public PlayableDirector zeroStarDirector;
+    public PlayableDirector oneStarDirector;
+    public PlayableDirector twoStarDirector;
+    public PlayableDirector threeStarDirector;
+    public SceneReference sceneReference;        // Note that this is assigned automatically when the level is created by the SceneMenuEditor class.
+    
+    // Called when target of level is achieved
+    public void CompleteLevel (float time)
+    {
+        panel.SetActive (true);
+        
+        int earnedStars = 0;
+
+        if (time <= sceneReference.threeStarTime)
+        {
+            earnedStars = 3;
+            threeStarDirector.Play();
+        }
+        else if (time <= sceneReference.twoStarTime)
+        {
+            earnedStars = 2;
+            twoStarDirector.Play();
+        }
+        else if (time <= sceneReference.oneStarTime)
+        {
+            earnedStars = 1;
+            oneStarDirector.Play();
+        }
+        else
+        {
+            zeroStarDirector.Play();
+        }
+        
+        if(sceneReference.earnedStars < earnedStars)
+            sceneReference.earnedStars = earnedStars;
+    }
+
+    // UI Button
+    public void ReloadLevel ()
+    {
+        if (sceneReference != null)
+        {
+            if(sceneReference.ignore)
+                Debug.LogError ("The ignore flag has been set for this scene and so it cannot be reloaded.");
+            else
+                sceneReference.ReloadLevel (screenFader);
+        }
+        else
+        {
+            screenFader.FadeOut(() => SceneManager.LoadSceneAsync (gameObject.scene.buildIndex));
+        }
+    }
+
+    // UI Button
+    public void LoadMenu ()
+    {
+        if (sceneReference != null)
+        {
+            if(sceneReference.ignore)
+                Debug.LogError ("The ignore flag has been set for this scene and so it cannot load its menu.");
+            else
+                sceneReference.LoadMenu (screenFader);
+        }
+    }
+}
